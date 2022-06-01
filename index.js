7492a3d8e3d8c020512ed755c799b46d91c3b423
Ulises Perez Acosta <u.perezacosta@gmail.com>
2022-06-01 05:31:00 -0400

    commit tree 30d0cc5a5501e3d40691690a3fb768a1c2eb34d2

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+
+
+
+
+const main = document.querySelector('#main').remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "Ulises is the champion"
+main.appendChild(victory) 
